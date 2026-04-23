647d6cd08bdc58f96bae8dc3b4fa55fb7d41da7b
EduardoBottosso <eduardobottosso@discente.ufg.br>
2026-04-23 01:16:40 -0300

    Update consumer2.py

--- consumer2.py
+++ consumer2.py
@@ -4,5 +4,5 @@ from const import *
 consumer = KafkaConsumer(TOPIC2, bootstrap_servers=[BROKER_ADDR + ':' + BROKER_PORT], auto_offset_reset='earliest')
 
 for msg in consumer:
-	final_text = msg.value.decode()
+    final_text = msg.value.decode()
     print('Final message from ' + TOPIC2 + ': ' + final_text)

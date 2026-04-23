b017bcd1ecbfca93a29f6c2b05c16d80948d78ee
EduardoBottosso <eduardobottosso@discente.ufg.br>
2026-04-23 00:45:28 -0300

    Update producer.py

--- producer.py
+++ producer.py
@@ -4,7 +4,7 @@ import sys
     
 producer = KafkaProducer(bootstrap_servers=[BROKER_ADDR + ':' + BROKER_PORT])
 for i in range(100):
-    msg = 'My ' + str(i) + 'message for topic ' + topic
+    msg = 'My ' + str(i) + 'message for topic ' + TOPIC1
     print ('Sending message: ' + msg)
     producer.send(TOPIC1, value=msg.encode())
 

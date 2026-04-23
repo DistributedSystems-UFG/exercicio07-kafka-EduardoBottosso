16e9b19675d69363661164309707a4d54577d3e1
EduardoBottosso <eduardobottosso@discente.ufg.br>
2026-04-23 00:39:09 -0300

    Update consumer.py

--- consumer.py
+++ consumer.py
@@ -1,19 +1,14 @@
-from kafka import KafkaConsumer
+from kafka import KafkaConsumer, KafkaProducer
 from const import *
 import sys
 
-# Create consumer: Option 1 -- only consume new events
-consumer = KafkaConsumer(bootstrap_servers=[BROKER_ADDR + ':' + BROKER_PORT])
+# aqui acrescentei a subscricao direta com o TOPIC1, que foi definido nas constantes:
+# e ele vai pegar publicacoes antigas tambem
+consumer = KafkaConsumer(TOPIC1, bootstrap_servers=[BROKER_ADDR + ':' + BROKER_PORT], auto_offset_reset='earliest')
+producer = KafkaProducer(bootstrap_servers=[BROKER_ADDR + ':' + BROKER_PORT])
 
-# Create consumer: Option 2 -- consume old events (uncomment to test -- and comment Option 1 above)
-#consumer = KafkaConsumer(bootstrap_servers=[BROKER_ADDR + ':' + BROKER_PORT], auto_offset_reset='earliest')
-
-try:
-  topic = sys.argv[1]
-except:
-  print ('Usage: python3 consumer <topic_name>')
-  exit(1)
-  
-consumer.subscribe([topic])
 for msg in consumer:
-    print (msg.value)
+    received_text = msg.value.decode()
+    processed_text = 'Processed ' + received_text.upper()
+    producer.send(TOPIC2, value=processed_text.encode())
+    producer.flush()
